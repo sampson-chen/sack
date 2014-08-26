b293053101b08bd0bfa624c155eb0d19a66409ae
Damien Garaud <damien.garaud@gmail.com>
2014-08-26 09:59:38 +0200

    Update the install script

--- install_sack.sh
+++ install_sack.sh
@@ -1,19 +1,18 @@
 #!/bin/bash
 #
 # @filename: install_sack.sh
-# 
+#
 # @author Sampson L. Chen (sampson-chen / slchen)
-# @date 2012-10-14
+# @date 2012--2014
 #
 # @description:
 # Sack auto-install script
-# 
+#
 # @usage:
 # run this script from where the git repo is cloned to.
 
 # sack installation variables
 sack__install_main=~/bin
-sack__install_sackrc=~/
 sack__install_cwd=$(pwd)
 
 # If the install directory doesn't exist, create it
@@ -22,7 +21,8 @@ if [[ ! -d "$sack__install_main" ]]; then
 fi
 
 cp $sack__install_cwd/sack $sack__install_main/
-chmod +x $sack__install_main/sack
+chmod 755 $sack__install_main/sack
 cp $sack__install_cwd/sag $sack__install_main/
-chmod +x $sack__install_main/sag
-cp $sack__install_cwd/.sackrc $sack__install_sackrc/
+chmod 755 $sack__install_main/sag
+cp $sack__install_cwd/F $sack__install_main/
+chmod 755 $sack__install_main/F

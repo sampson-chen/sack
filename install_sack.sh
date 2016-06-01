c24cb0cdfc1925fde4316c305d9aeb0f77b0bace
Kevin Ernst <ernstki@mail.uc.edu>
2016-06-01 20:56:28 +0200

    Actually create a .sackrc (again) if it doesn't exist
    Reverts a dumb mistake I made with c60e48a, where if you didn't
    have a ~/.sackrc, it wouldn't bother to create one at all.
    
    Partially addresses the issue in #32.

--- install_sack.sh
+++ install_sack.sh
@@ -12,13 +12,14 @@
 # run this script from where the git repo is cloned to.
 
 # sack installation variables
-sack__install_main=~/bin
-sack__install_sackrc=~/
+sack__install_main=$HOME/bin
+sack__install_sackrc=$HOME
 sack__install_cwd=$(pwd)
 
 # If the install directory doesn't exist, create it
 if [[ ! -d "$sack__install_main" ]]; then
-    mkdir $sack__install_main
+    echo "Creating '$sack__install_main'..."
+    mkdir "$sack__install_main"
 fi
 
 # If ack is not installed, install it
@@ -28,23 +29,34 @@ fi
 
 # No install script for ag, so let it be a TODO
 
-cp $sack__install_cwd/sack $sack__install_main/
-chmod +x $sack__install_main/sack
-cp $sack__install_cwd/sag $sack__install_main/
-chmod +x $sack__install_main/sag
+echo "Copying 'sack' to '$sack__install_main/sack'..."
+cp "$sack__install_cwd/sack" "$sack__install_main"
+chmod +x "$sack__install_main/sack"
+
+echo "Copying 'sag' to '$sack__install_main/sag'..."
+cp "$sack__install_cwd/sag" "$sack__install_main"
+chmod +x "$sack__install_main/sag"
 
 if [[ -f "$sack__install_sackrc/.sackrc" ]]; then
     echo >&2
-    echo "It seems you already have a ~/.sackrc." >&2
+    echo "It seems you already have a .sackrc from a previous install." >&2
     echo >&2
     echo "Overwrite this with a fresh copy from the source distribution" >&2
     echo "(losing your customizations)?" >&2
     echo >&2
     read -p "(Ctrl+C to quit) y/[N]? " ANS
     if [[ $ANS =~ ^[Yy] ]]; then
-        cp $sack__install_cwd/.sackrc $sack__install_sackrc/
+        echo
+        echo "Overwriting your existing rcfile at '$sack__install_main/.sackrc'..."
+        cp "$sack__install_cwd/.sackrc" "$sack__install_sackrc"
     else
         echo
-        echo "Okay, not overwriting your existing ~/.sackrc."
+        echo "Okay, not overwriting your existing .sackrc."
     fi
+else
+    echo "Creating new rcfile at '$sack__install_main/.sackrc'..."
+    cp "$sack__install_cwd/.sackrc" "$sack__install_sackrc"
 fi
+
+echo
+echo "All done."
